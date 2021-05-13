191935dde94270fe1737ad990ceffdeadffda85b
Abhishek Thapa <abhi2010thapa@gmail.com>
2021-05-13 19:43:28 +0545

    day3 1st commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', views.post_list, name='post_list')
+    #path('', views.index, name='index'),
+    #path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'post_list.html', {})
 def home(request):
     return HttpResponse('Hey')
 
