2ab3764c1a55cc99d583d0c50dfc10e29ef32a54
Abhishek Thapa <abhi2010thapa@gmail.com>
2021-05-14 19:45:52 +0545

    day4

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
 
     
 def home(request):
