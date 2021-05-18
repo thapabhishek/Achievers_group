82ee23ab74ae0edcfaa279cd2ada4a8874205c47
Abhishek Thapa <abhi2010thapa@gmail.com>
2021-05-18 09:07:36 +0545

    day5, templates views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -6,6 +6,10 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
 
     
 def home(request):
